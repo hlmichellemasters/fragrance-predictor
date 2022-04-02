d4e3d1bbb44d07f8394098d40207a84b1d2970c5
Michelle Masters <s-heaven-leigh.maste@lwtech.edu>
2022-04-02 15:33:34 -0500

    amazon settings

--- fragrance_project/settings.py
+++ fragrance_project/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = 'static'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
