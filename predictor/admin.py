831352823214cba9ff1e21c017454794856d2282
Michelle Masters <s-heaven-leigh.maste@lwtech.edu>
2022-04-24 03:25:29 -0500

    redeploy

--- predictor/admin.py
+++ predictor/admin.py
@@ -6,19 +6,22 @@ from import_export.admin import ImportExportMixin
 @admin.register(Preference)
 class PreferenceAdmin(ImportExportMixin, admin.ModelAdmin):
     model = Preference
-    list_display = ('user', 'perfume', 'love', 'comment', 'review_date')
-    list_filter = ['user', 'perfume', 'love']
-    search_fields = ['comment']
+    list_display = ('id', 'user', 'perfume', 'love', 'comment', 'review_date')
+    list_filter = ('user', 'perfume', 'love')
+    search_fields = ('id', 'comment')
 
 
 @admin.register(Perfume)
 class PerfumeAdmin(ImportExportMixin, admin.ModelAdmin):
     model = Perfume
-    list_display = ('name', 'house', 'description', 'added_date', 'added_by')
-    list_filter = ['added_by', 'house']
-    search_fields = ['name', 'description']
+    list_display = ('id', 'name', 'house', 'description', 'added_date', 'added_by')
+    list_filter = ('added_by', 'house')
+    search_fields = ('id', 'name', 'description')
 
 
 @admin.register(House)
 class HouseAdmin(ImportExportMixin, admin.ModelAdmin):
     model = House
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    
